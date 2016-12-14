22551f9db8927bc3571dbaf00abffba7b4c346f0
kushalpatel2010 <kushalpatel2010@gmail.com>
2016-12-14 14:25:33 +0530

    Intial Commit
    Signal R

---
+++ PushNotification/NotificationComponent.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+namespace PushNotification
+{
+    public class NotificationComponent
+    {
+        public void RegisterNotification(DateTime currentTime)
+        {
+            string conStr = ConfigurationManager.ConnectionStrings["sqlConString"].ConnectionString;
+            string sqlCommand = "select [ContactId], [ContactName] [ContactNo] from [dbo].[Contacts] where [AddedOn] > @AddedOn";
+
+            using(SqlConnection con=new SqlConnection(conStr))
+            {
+                SqlCommand cmd=new SqlCommand(sqlCommand,con);
+                cmd.Parameters.AddWithValue("AddedOn", currentTime);
+                if (con.State != System.Data.ConnectionState.Open)
+                {
+                    con.Open();
+                }
+                cmd.Notification = null;
+                SqlDependency sqlDep = new SqlDependency(cmd);
+                sqlDep.OnChange += sqlDep_OnChange;
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+ 
+                }
+            }
+        }
+
+        void sqlDep_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            if (e.Type == SqlNotificationType.Change)
+            {
+                SqlDependency sqlDep = sender as SqlDependency;
+                sqlDep.OnChange -= sqlDep_OnChange;
+                var notificationHub = GlobalHost.ConnectionManager.GetHubContext<notificationHub>();
+                notificationHub.Clients.All.notify("added");
+                RegisterNotification(DateTime.Now);
+            }
+        }
+
+        public List<Contact> GetContacts(DateTime afterDate)
+        {
+            using (MyPushNotificationEntities dc = new MyPushNotificationEntities())
+            {
+                return dc.Contacts.Where(x=>x.AddedOn>afterDate).OrderByDescending(a=>a.AddedOn).ToList();
+            }
+        }
+    }
+}
---
+++ PushNotification/notificationHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace PushNotification
+{
+    public class notificationHub : Hub
+    {
+        //public void Hello()
+        //{
+        //    Clients.All.hello();
+        //}
+
+        public void Send(string name, string message)
+        {
+            
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
